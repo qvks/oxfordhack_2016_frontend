c6364c6f01061f9d332d010dc6426686be0f21af
Alessandro Bonardi <ab9515@ic.ac.uk>
2016-11-19 20:48:35 +0000

    Started javascript to fetch data from form

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Alessandro on 19/11/2016.
+ */
+
+var URL;
+var location;
+
+function getURL(){
+    URL = document.getElementById("usr").value;
+    location = document.getElementById("pos").value;
+    console.log(URL);
+    console.log(location);
+}
